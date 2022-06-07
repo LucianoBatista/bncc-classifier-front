a23228fd003eab02835d77c6d67ffc45dba28f99
Luba <luciano.eq@outlook.com>
2022-06-07 17:01:34 -0300

    :sparkles: adjusting the principal page.

--- app/main.py
+++ app/main.py
@@ -3,10 +3,34 @@ import streamlit as st
 import streamlit.components.v1 as components
 import requests
 import json
+import streamlit_modal as modal
+
 
 st.write("# Classificador BNCC")
 
-message_text = st.text_input("Enter a message for spam evaluation")
+st.markdown(
+    """
+    --- 
+    Este é um MVP (Minimum Viable Product) resultado do trabalho do **Grupo de Educação**: 
+    - 👦 [Pedro](https://www.linkedin.com/in/pedromoreau/)
+    - 👧 [Brisa](https://www.linkedin.com/in/brisarosatti/)
+    - 👦 [Will](https://www.linkedin.com/in/wilson-font/)
+    - 👦 [Luciano](https://www.linkedin.com/in/lucianobatistads/).
+    ---
+    Nosso classificador recebe uma *questão escrita* no campo abaixo e retorna a classificação da mesma dentro das seguintes categorias:
+    
+    ### Área do conhecimento
+
+    - Matemática, Arte, Biologia, Ciências, Educação Física, Ensino Religioso, Física, Geografia, História, Inglês, Língua Portuguesa e Química
+
+    ### Grau de escolaridade
+
+    - Ensino Fundamental I, Ensino Fundamental II, Ensino Médio
+    ---
+    """
+)
+
+message_text = st.text_input("👇 Digite aqui a sua questão!")
 
 
 def predict(text):
@@ -30,9 +54,23 @@ def predict(text):
 
 if message_text != "":
 
-    print("We have a text input...")
-    prediction = st.button("Predict!")
+    counting_text_letters = len(message_text)
 
-    if prediction:
-        response = predict(message_text)
-        st.write(json.loads(response.text))
+    if counting_text_letters > 10:
+
+        prediction = st.button("Predict!")
+
+        if prediction:
+            response = predict(message_text)
+            response_dict = json.loads(response.text)
+            # st.write(json.loads(response.text))
+            bncc_know_area = response_dict.get("bncc_know_area")
+            grau_escolaridade = response_dict.get("school_phase")
+
+            st.markdown(f"> **Área do Conhecimento**: {bncc_know_area[0]}")
+            st.markdown(f"> **Grau de Escolaridade**: {grau_escolaridade[0]}")
+
+    else:
+        st.markdown(
+            "**:warning:Desculpe, não possui caracteres suficientes para realizar a predição.**"
+        )
